d852df2e776e9de6e4fc00fca6a94d8d386bb8e1
AtiaZ <zatia@exoplatform.com>
2016-03-04 12:34:49 +0100

    No notification when we mention a user who has a username which begins with a number

--- component/core/src/main/java/org/exoplatform/social/core/storage/impl/ActivityStorageImpl.java
+++ component/core/src/main/java/org/exoplatform/social/core/storage/impl/ActivityStorageImpl.java
@@ -102,7 +102,7 @@ public class ActivityStorageImpl extends AbstractStorage implements ActivityStor
   /** Logger */
   private static final Log LOG = ExoLogger.getLogger(ActivityStorageImpl.class);
   private static final Pattern MENTION_PATTERN = Pattern.compile("@([^\\s]+)|@([^\\s]+)$");
-  public static final Pattern USER_NAME_VALIDATOR_REGEX = Pattern.compile("^[\\p{L}][\\p{L}._\\-\\d]+$");
+  public static final Pattern USER_NAME_VALIDATOR_REGEX = Pattern.compile("^[\\p{Alnum}][\\p{Alnum}._\\-\\d]+$");
   private ActivityStorage activityStorage;
 
   private final SortedSet<ActivityProcessor> activityProcessors;
