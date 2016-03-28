8231dce6c59a4f70ecaf1119e86887619c66b2f1
wkhessairi <wkhessairi@exoplatform.com>
2016-03-28 16:13:04 +0200

    [SOC-5276]Redirect to "join request" when entering URL for Hidden/Validation space

--- component/core/src/main/java/org/exoplatform/social/core/space/SpaceAccessType.java
+++ component/core/src/main/java/org/exoplatform/social/core/space/SpaceAccessType.java
@@ -89,7 +89,7 @@ public enum SpaceAccessType {
       if (space == null) {
         result = true;
       } else if (space != null) {
-        result = "hidden".equals(space.getVisibility()) && !getSpaceService().isMember(space, remoteId);
+        result = "hidden".equals(space.getVisibility()) && !getSpaceService().isMember(space, remoteId) && "close".equals(space.getRegistration());
       }
       
       return result;
