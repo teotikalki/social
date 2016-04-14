97b656c7df7479b2845b7ff8a4c36aec2cb2efe7
boubaker <bkhanfir@exoplatform.com>
2016-04-14 14:24:30 +0100

    SOC-5286: Optimize stored Social data

--- component/core/src/main/java/org/exoplatform/social/core/updater/SocialMixinCleanerUpgradePlugin.java
+++ component/core/src/main/java/org/exoplatform/social/core/updater/SocialMixinCleanerUpgradePlugin.java
@@ -24,22 +24,23 @@ import java.sql.Statement;
 import javax.jcr.ItemNotFoundException;
 import javax.jcr.Node;
 import javax.jcr.Session;
+import javax.servlet.ServletContext;
 import javax.transaction.NotSupportedException;
 import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
 
 import org.apache.commons.lang.StringUtils;
+
 import org.exoplatform.commons.upgrade.UpgradeProductPlugin;
 import org.exoplatform.commons.version.util.VersionComparator;
 import org.exoplatform.container.PortalContainer;
+import org.exoplatform.container.RootContainer.PortalContainerPostInitTask;
 import org.exoplatform.container.xml.InitParams;
-import org.exoplatform.container.xml.ValueParam;
 import org.exoplatform.services.jcr.RepositoryService;
 import org.exoplatform.services.jcr.config.WorkspaceEntry;
 import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.core.WorkspaceContainerFacade;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
-import org.exoplatform.services.jcr.impl.core.RepositoryImpl;
 import org.exoplatform.services.jcr.impl.core.SessionImpl;
 import org.exoplatform.services.jcr.impl.storage.jdbc.JDBCWorkspaceDataContainer;
 import org.exoplatform.services.jcr.impl.util.jdbc.DBInitializerHelper;
@@ -53,41 +54,51 @@ import org.exoplatform.services.transaction.TransactionService;
  */
 public class SocialMixinCleanerUpgradePlugin extends UpgradeProductPlugin {
 
-  private static final int TRANSACTION_TIMEOUT_IN_SECONDS = 3600;
-  private static final String PLUGIN_PROCEED_VERSION = "4.3.0";
-  private static final String QUERY = "select distinct PARENT_ID as id from JCR_SITEM" + " where container_name = 'social' and" + " (  NAME= '[http://www.exoplatform.com/jcr/exo/1.0]name' "
-      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]title'" + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]titlePublished'"
-      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]index'" + " or NAME= '[http://www.exoplatform.com/jcr/publication/1.1/]liveDate'"
-      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]owner'" + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]dateCreated'"
-      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]dateModified'" + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]lastModifiedDate'"
-      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]lastModifier'" + ");";
+  private static final int         TRANSACTION_TIMEOUT_IN_SECONDS = 3600;
 
-  private static final Log log = ExoLogger.getLogger(SocialMixinCleanerUpgradePlugin.class);
-  private final static String[] MIXIN_NAMES = new String[] { "exo:modify", "exo:datetime", "exo:owneable", "exo:sortable" };
+  private static final String      PLUGIN_PROCEED_VERSION         = "4.3.0";
 
-  private static final int FETCH_SIZE = 1000;
+  private static final String      QUERY                          = "select distinct PARENT_ID as id from JCR_SITEM"
+                                                                      + " where container_name = 'social'"
+                                                                      + " and (  NAME= '[http://www.exoplatform.com/jcr/exo/1.0]name' "
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]title'"
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]titlePublished'"
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]index'"
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/publication/1.1/]liveDate'"
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]owner'"
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]dateCreated'"
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]dateModified'"
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]lastModifiedDate'"
+                                                                      + " or NAME= '[http://www.exoplatform.com/jcr/exo/1.0]lastModifier'"
+                                                                      + ");";
 
-  private final PortalContainer portalContainer;
-  private final RepositoryService repositoryService;
+  private static final Log         log                            = ExoLogger.getLogger(SocialMixinCleanerUpgradePlugin.class);
+
+  private static final String[]    MIXIN_NAMES                    = new String[] { "exo:modify", "exo:datetime", "exo:owneable",
+      "exo:sortable"                                             };
+
+  private static final String      WORKSPACE_NAME                 = "social";
+
+  private static final int         FETCH_SIZE                     = 1000;
+
+  private final PortalContainer    portalContainer;
+
+  private final RepositoryService  repositoryService;
+
   private final TransactionService txService;
-  private final String workspaceName;
 
-  public SocialMixinCleanerUpgradePlugin(PortalContainer portalContainer, RepositoryService repositoryService, TransactionService txService, InitParams initParams) {
+  public SocialMixinCleanerUpgradePlugin(PortalContainer portalContainer,
+                                         RepositoryService repositoryService,
+                                         TransactionService txService,
+                                         InitParams initParams) {
     super(initParams);
-    ValueParam workspaceValueParam = initParams.getValueParam("workspace");
-    if (workspaceValueParam != null) {
-      workspaceName = workspaceValueParam.getValue();
-    } else {
-      workspaceName = null;
-    }
-
     this.repositoryService = repositoryService;
     this.txService = txService;
     this.portalContainer = portalContainer;
   }
 
   /**
-   * @return true if current version is uppper or equals to than 4.3.1, else
+   * @return true if current version is uppper or equals to than 4.3.0, else
    *         return false
    */
   @Override
@@ -100,155 +111,154 @@ public class SocialMixinCleanerUpgradePlugin extends UpgradeProductPlugin {
    */
   @Override
   public void processUpgrade(String oldVersion, String newVersion) {
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        while (!portalContainer.isStarted()) {
-          try {
-            // Wait 10 seconds
-            Thread.sleep(10000);
-          } catch (InterruptedException e) {
-            log.warn("An interruption on wait was thrown, ignore it.");
+    PortalContainer.addInitTask(portalContainer.getPortalContext(), new PortalContainerPostInitTask() {
+      public void execute(ServletContext context, PortalContainer portalContainer) {
+        // Execute the task in an asynchrounous way
+        new Thread(new Runnable() {
+          @Override
+          public void run() {
+            // Wait for 10 seconds until the server is fully started.
+            try {
+              Thread.sleep(10000);
+            } catch (InterruptedException e) {
+              // Nothing to do
+            }
+            doMigration();
           }
-        }
+        }, "SocialMixinCleanerUpgradePlugin").start();
+      }
+    });
+  }
 
-        log.info("Start migration");
+  private void doMigration() {
+    log.info("Start migration");
 
-        Connection jdbcConn = null;
-        SessionProvider sessionProvider = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        ManageableRepository currentRepo = null;
+    Connection jdbcConn = null;
+    SessionProvider sessionProvider = null;
+    Statement stmt = null;
+    ResultSet rs = null;
 
-        long totalCount = 0;
+    long totalCount = 0;
+
+    try {
+      ManageableRepository currentRepository = repositoryService.getCurrentRepository();
+      WorkspaceContainerFacade containerFacade = currentRepository.getWorkspaceContainer(WORKSPACE_NAME);
+      if (containerFacade == null) {
+        throw new IllegalStateException("Workspace with name '" + WORKSPACE_NAME + "' wasn't found");
+      }
+
+      // Get JCR Session
+      sessionProvider = SessionProvider.createSystemProvider();
+      Session session = sessionProvider.getSession(WORKSPACE_NAME, currentRepository);
+
+      // Get JDBC Result Set
+      JDBCWorkspaceDataContainer dataContainer = (JDBCWorkspaceDataContainer) containerFacade.getComponent(JDBCWorkspaceDataContainer.class);
+      jdbcConn = dataContainer.connFactory.getJdbcConnection();
+      stmt = jdbcConn.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY);
+      stmt.setFetchSize(FETCH_SIZE);
+      stmt.setQueryTimeout(TRANSACTION_TIMEOUT_IN_SECONDS * 24);
+
+      WorkspaceEntry wEntry = getWorkspaceEntry(currentRepository);
+      String query = QUERY.replace("JCR_SITEM", DBInitializerHelper.getItemTableName(wEntry));
+      query = query.replace("'social'", "'" + WORKSPACE_NAME + "'");
+      rs = stmt.executeQuery(query);
+
+      // Begin transaction
+      UserTransaction transaction = beginTransaction();
+      while (rs.next()) {
+        String id = rs.getString(1);
+        id = id.replaceAll(WORKSPACE_NAME, "");
 
         try {
-          WorkspaceContainerFacade containerFacade = repositoryService.getCurrentRepository().getWorkspaceContainer(workspaceName);
-          if (containerFacade == null) {
-            throw new IllegalStateException("Workspace with name '" + workspaceName + "' wasn't found");
-          }
-
-          // Get JCR Session
-          sessionProvider = SessionProvider.createSystemProvider();
-          currentRepo = ((RepositoryImpl) repositoryService.getCurrentRepository());
-          Session session = sessionProvider.getSession(workspaceName, currentRepo);
-
-          // Get JDBC Result Set
-          JDBCWorkspaceDataContainer dataContainer = (JDBCWorkspaceDataContainer) containerFacade.getComponent(JDBCWorkspaceDataContainer.class);
-          jdbcConn = dataContainer.connFactory.getJdbcConnection();
-          stmt = jdbcConn.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY);
-          stmt.setFetchSize(FETCH_SIZE);
-          stmt.setQueryTimeout(TRANSACTION_TIMEOUT_IN_SECONDS * 24);
-
-          WorkspaceEntry wEntry = getWorkspaceEntry(currentRepo);
-          String query = QUERY.replace("JCR_SITEM", DBInitializerHelper.getItemTableName(wEntry));
-          query = query.replace("'social'", "'" + workspaceName + "'");
-          rs = stmt.executeQuery(query);
-
-          // Begin transaction
-          UserTransaction transaction = beginTransaction();
-          while (rs.next()) {
-            String id = rs.getString(1);
-            id = id.replaceAll(workspaceName, "");
-
-            try {
-              Node node = null;
-              try {
-                // Test if node exists
-                try {
-                  node = ((SessionImpl) session).getNodeByIdentifier(id);
-                } catch (ItemNotFoundException e) {
-                  log.warn("Item not found with id: '{}'", id);
-                  continue;
-                }
-                // Avoid changing Root Node of Workspace
-                if (node.getPath().equals("/")) {
-                  continue;
-                }
-
-                boolean proceeded = false;
-
-                // FIXME: JCR-2442, remove mixin on node using a property
-                // definition of name "*" leads to an exception.
-                // That's why we exclude 'soc:profiledefinition'
-                if (node.isNodeType("soc:profiledefinition")) {
-                  log.debug("Ignore id: {}, nodetype = {}.", id, node.getPrimaryNodeType().getName());
-                } else {
-                  // Remove all mixins from nodes
-                  for (String mixinName : MIXIN_NAMES) {
-                    if (StringUtils.isBlank(mixinName) || !node.isNodeType(mixinName)) {
-                      continue;
-                    }
-                    // Ignore deletion of 'exo:datetime' and 'exo:modify' from nodes of type 'soc:spaceref'
-                    // Those mixins are used to sort spaces by access time
-                    if (node.isNodeType("soc:spaceref") && (("exo:datetime".equals(mixinName) || "exo:modify".equals(mixinName)))) {
-                      log.debug("Ignore id: {}, nodetype = {}, remove mixin '{}'", id, node.getPrimaryNodeType().getName(), mixinName);
-                    } else {
-                      log.debug("Proceed id: {}, nodetype = {}, remove mixin {}", id, node.getPrimaryNodeType().getName(), mixinName);
-                      node.removeMixin(mixinName);
-                      node.save();
-                      proceeded = true;
-                    }
-                  }
-                }
-                if(proceeded) {
-                  totalCount++;
-                }
-              } catch (Exception e) {
-                log.warn("Error updating node with id " + id, e);
-                if (node != null) {
-                  node.refresh(false);
-                }
-              }
-              if (totalCount > 0 && totalCount % FETCH_SIZE == 0) {
-                session.save();
-                transaction.commit();
-                log.info("Migration in progress, proceeded nodes count = {}", totalCount);
-
-                transaction = beginTransaction();
-              }
-            } catch (Exception e) {
-              transaction.rollback();
-              transaction = beginTransaction();
-            }
-          }
-          session.save();
-          transaction.commit();
-        } catch (Exception e) {
-          log.error(e);
-        } finally {
-          if (sessionProvider != null) {
-            sessionProvider.close();
-          }
+          Node node = null;
           try {
-            if (rs != null) {
-              rs.close();
+            // Test if node exists
+            try {
+              node = ((SessionImpl) session).getNodeByIdentifier(id);
+            } catch (ItemNotFoundException e) {
+              log.warn("Item not found with id: '{}'", id);
+              continue;
             }
-            if (stmt != null) {
-              stmt.close();
+            // Avoid changing Root Node of Workspace
+            if (node.getPath().equals("/")) {
+              continue;
             }
-            if (jdbcConn != null) {
-              jdbcConn.close();
+
+            boolean proceeded = false;
+
+            // Remove all mixins from nodes
+            for (String mixinName : MIXIN_NAMES) {
+              if (StringUtils.isBlank(mixinName) || !node.isNodeType(mixinName)) {
+                continue;
+              }
+              // Ignore deletion of 'exo:datetime' and 'exo:modify' from
+              // nodes of type 'soc:spaceref'
+              // Those mixins are used to sort spaces by access time
+              if (node.isNodeType("soc:spaceref") && (("exo:datetime".equals(mixinName) || "exo:modify".equals(mixinName)))) {
+                log.debug("Ignore id: {}, nodetype = {}, remove mixin '{}'", id, node.getPrimaryNodeType().getName(), mixinName);
+              } else {
+                log.debug("Proceed id: {}, nodetype = {}, remove mixin {}", id, node.getPrimaryNodeType().getName(), mixinName);
+                node.removeMixin(mixinName);
+                node.save();
+                proceeded = true;
+              }
             }
-          } catch (SQLException e) {
-            log.error("Cannot close connection", e);
+            if (proceeded) {
+              totalCount++;
+            }
+          } catch (Exception e) {
+            log.warn("Error updating node with id " + id, e);
+            if (node != null) {
+              node.refresh(false);
+            }
           }
+          if (totalCount > 0 && totalCount % FETCH_SIZE == 0) {
+            session.save();
+            transaction.commit();
+            log.info("Migration in progress, proceeded nodes count = {}", totalCount);
+
+            transaction = beginTransaction();
+          }
+        } catch (Exception e) {
+          transaction.rollback();
+          transaction = beginTransaction();
         }
-        log.info("Migration finished, proceeded nodes count = {}", totalCount);
       }
-    }, "SocialMixinCleanerUpgradePlugin").start();
+      session.save();
+      transaction.commit();
+    } catch (Exception e) {
+      log.error(e);
+    } finally {
+      if (sessionProvider != null) {
+        sessionProvider.close();
+      }
+      try {
+        if (rs != null) {
+          rs.close();
+        }
+        if (stmt != null) {
+          stmt.close();
+        }
+        if (jdbcConn != null) {
+          jdbcConn.close();
+        }
+      } catch (SQLException e) {
+        log.error("Cannot close connection", e);
+      }
+    }
+    log.info("Migration finished, proceeded nodes count = {}", totalCount);
   }
 
   private WorkspaceEntry getWorkspaceEntry(ManageableRepository currentRepo) {
     WorkspaceEntry wEntry = null;
     for (WorkspaceEntry entry : currentRepo.getConfiguration().getWorkspaceEntries()) {
-      if (entry.getName().equals(workspaceName)) {
+      if (entry.getName().equals(WORKSPACE_NAME)) {
         wEntry = entry;
         break;
       }
     }
     if (wEntry == null) {
-      throw new IllegalStateException("Worksapce \"" + workspaceName + "\" was not found.");
+      throw new IllegalStateException("Worksapce \"" + WORKSPACE_NAME + "\" was not found.");
     }
     return wEntry;
   }
